ff51c79aedab5ad0c8f188984cdcb83cd6dde641
Necro <mfabioalfarizie1@gmail.com>
2023-12-17 00:51:49 +0700

    UI update

--- Script.js
+++ Script.js
@@ -1,16 +1,31 @@
-// Toggle class active
-const navbarNav = document.querySelector(".navbar-nav");
+// Toggle class active hamburgerMenu
+const navbarNav = document.querySelector('.navbar-nav');
 
 // When Hamburger-menu clicked
-document.querySelector("#hamburger-menu").onclick = () => {
+document.querySelector('#hamburger-menu').onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-// Click outside the side bar for close the nav
-const hamburger = document.querySelector("#hamburger-menu");
+// Toggle class active searchBar
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box')
+
+document.querySelector('#search-btn').onclick = (e) => {
+  searchForm.classList.toggle('active');
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// Click outside of an element
+const hm = document.querySelector('#hamburger-menu');
+const sb =document.querySelector('#search-btn');
 
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
